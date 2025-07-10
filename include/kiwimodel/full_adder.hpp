86b83246ca4cab7fd0c664f4caad85432ec720c2
Miguel Villa Floran <miguel.villafloran@gmail.com>
2025-07-09 23:54:54 -0700

    Refactor

--- include/kiwimodel/full_adder.hpp
+++ include/kiwimodel/full_adder.hpp
@@ -15,32 +15,29 @@ SC_MODULE(full_adder) {
   sc_out<bool> sum; // Sum output
   sc_out<bool> cout; // Carry output
 
-  // Process declaration
-  void compute();
-
   // Constructor
   SC_CTOR(full_adder) {
     // Register the process
     SC_METHOD(compute);
     sensitive << a << b << cin;
   }
+
+  // Process declaration
+  void compute() {
+    // Full adder logic
+    // Sum = A XOR B XOR Cin
+    // Cout = (A AND B) OR (Cin AND (A XOR B))
+
+    bool a_val = a.read();
+    bool b_val = b.read();
+    bool cin_val = cin.read();
+
+    bool sum_val = a_val ^ b_val ^ cin_val;
+    bool cout_val = (a_val & b_val) | (cin_val & (a_val ^ b_val));
+
+    sum.write(sum_val);
+    cout.write(cout_val);
+  }
 };
-
-// Process implementation
-void full_adder::compute() {
-  // Full adder logic
-  // Sum = A XOR B XOR Cin
-  // Cout = (A AND B) OR (Cin AND (A XOR B))
-
-  bool a_val = a.read();
-  bool b_val = b.read();
-  bool cin_val = cin.read();
-
-  bool sum_val = a_val ^ b_val ^ cin_val;
-  bool cout_val = (a_val & b_val) | (cin_val & (a_val ^ b_val));
-
-  sum.write(sum_val);
-  cout.write(cout_val);
-}
 
 } // namespace kiwimodel
